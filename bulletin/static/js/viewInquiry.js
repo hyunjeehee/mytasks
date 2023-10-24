a49fe58352fd3528968b1959df638271a09b82f0
hyunjeehee <jhhyun@datadesign.engineering>
2023-10-24 22:45:12 +0900

    map함수 추가

--- bulletin/static/js/viewInquiry.js
+++ bulletin/static/js/viewInquiry.js
@@ -70,46 +70,45 @@ document.addEventListener("DOMContentLoaded", function () {
             let listTbody = `<tr><td colspan="5">등록된 문의글이 없습니다.</td></tr>`;
             $('#listTbody').html(listTbody);
             $('#Result').show();
-        } else {
-
-            let listTbody = '';
-
-            for (let item of data){
-                listTbody += `<tr>
-                                <td>${item.id}</td>
-                                <td>${item.content}</td>
-                                <td>${item.password}</td>
-                                <td>${item.create_time}</td>
-                                <td>${item.update_time}</td>
-                                <td><button class='delete_btn' type="button" name='${item.id}'>삭제</button></td>
-                             </tr>`;
-            }
-
-            $('#listTbody').html(listTbody);
-            
-
-            
-            const delete_btn = document.querySelectorAll('.delete_btn');
-            delete_btn.forEach((btn) =>{
-                btn.addEventListener('click', function (e) {
-                    e.preventDefault();
-                    
-                    // var inquiryId = document.getElementById(""); 안되는 이유
-                    console.log('삭제 버튼');
-                    console.log(this.name);
-                
-                    fetch(`/inquiry/${this.name}`, {
-                        method: 'DELETE',
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    })
-                    .then((response) => response.json())
-                    .then(() => location.reload());
-                });
-            });
         }
-    });
+//else 넣으면 왜 안되지..?
+        let listTbody = data.map((item) => {
+            return `<tr>
+                <td>${item.id}</td>
+                <td>${item.content}</td>
+                <td>${item.password}</td>
+                <td>${item.create_time}</td>
+                <td>${item.update_time}</td>
+                <td><button class='delete_btn' type="button" name='${item.id}'>삭제</button></td>
+            </tr>`;
+        });
+                
+        console.log(listTbody);
+
+        $('#listTbody').html(listTbody);
+                
+                
+        const delete_btn = document.querySelectorAll('.delete_btn');
+        delete_btn.forEach((btn) =>{
+            btn.addEventListener('click', function (e) {
+                e.preventDefault();
+                
+                // var inquiryId = document.getElementById(""); 안되는 이유
+                console.log('삭제 버튼');
+                console.log(this.name);
+            
+                fetch(`/inquiry/${this.name}`, {
+                    method: 'DELETE',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                .then((response) => response.json())
+                .then(() => location.reload());
+            });
+        });
+            
+        });
          
 });
 
